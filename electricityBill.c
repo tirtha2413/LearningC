5515a52b882442aaa0e8163d5f00c72214440330
Tirthankar Chakraborty <tirthankar.c07@gmail.com>
2021-11-08 08:21:00 +0530

    Update electricityBill.c

--- electricityBill.c
+++ electricityBill.c
@@ -1,3 +1,9 @@
+/*An Electricity board charges the following rates for use of electricity.
+For the First 200 units : Rs 1 per unit
+For the next 100 units : Rs 1.5 per unit
+Beyond 300 units : Rs 2 Per unit.
+Write a C Program to read no of unit consumed and print out total charge amount.*/
+
 #include <stdio.h>
 void main()
 {
