479ece7aece721cd853e9f1bea0bfc533ffd6aa4
Tirthankar Chakraborty <tirthankar.c07@gmail.com>
2021-11-08 08:29:50 +0530

    Update grossSalary.c

--- grossSalary.c
+++ grossSalary.c
@@ -1,10 +1,6 @@
-/*Write a C program to accept the basic salary of an employee from the user. Calculate the gross
-salary on the following basis:
-Basic               HRA DA
-1 - 4000            10% 50%
-4001 - 8000         20% 60%
-8001 - 12000        25% 70%
-12000 and above    30% 80% */
+/*Ramesh’s basic salary is input through the keyboard. His dearness
+allowance is 40% of basic salary, and house rent allowance is 20% of
+basic salary. Write a program to calculate his gross salary */
 #include <stdio.h>
 void main()
 {
