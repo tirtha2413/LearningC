404155f6c55e345ba8e12ac0d469e8b819bd3045
Tirthankar Chakraborty <tirthankar.c07@gmail.com>
2021-11-08 08:30:40 +0530

    Update paperSizes.c

--- paperSizes.c
+++ paperSizes.c
@@ -1,3 +1,8 @@
+/*Paper of size A0 has dimensions 1189 mm x 841 mm. Each
+subsequent size A(n) is defined as A(n-1) cut in half parallel to its
+shorter sides. Thus paper of size A1 would have dimensions 841
+mm x 594 mm. Write a program to calculate and print paper sizes
+A0, A1, A2, … A8*/ 
 #include <stdio.h>
 void main()
 {
