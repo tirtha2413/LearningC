0ec1df7c5b96516072c36514b366a98f386980a8
Tirthankar Chakraborty <tirthankar.c07@gmail.com>
2021-11-08 08:22:19 +0530

    Update grossSalary.c

--- grossSalary.c
+++ grossSalary.c
@@ -1,3 +1,10 @@
+/*Write a C program to accept the basic salary of an employee from the user. Calculate the gross
+salary on the following basis:
+Basic               HRA DA
+1 - 4000            10% 50%
+4001 - 8000         20% 60%
+8001 - 12000        25% 70%
+12000 and above    30% 80% */
 #include <stdio.h>
 void main()
 {
