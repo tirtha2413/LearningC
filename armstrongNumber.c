57c907c5e008273bbbdd46885613c4e643613a9d
Tirthankar Chakraborty <tirthankar.c07@gmail.com>
2021-11-08 08:19:59 +0530

    Update armstrongNumber.c

--- armstrongNumber.c
+++ armstrongNumber.c
@@ -1,3 +1,6 @@
+/*A number is called an Armstrong number if the sum of the cubes of the digits of the number is
+equal to the number. For example 153 = 1^3 + 5^3 + 3^3. Write a C program that asks the user to
+enter a number and returns if it is Armstrong or not.*/
 #include<stdio.h>
 #include<math.h>
 void main()
